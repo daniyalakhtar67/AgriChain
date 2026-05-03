51790ba33a4e78adacd55b475aca73cdeaeb52ce
daniyalaakhtar67 <daniyalakhtarpatoojo125@gmail.com>
2026-05-03 00:05:22 -0700

    Payment method added

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: BuyerDashboard(buyerName: 'daniyal'),
+      home: ShopkeeperDashboard(shopkeeperName: 'D'),
       routes: {
         '/welcome': (context) => const WelcomeScreen(),
         '/buyerRegister': (context) => const BuyerRegister(),
